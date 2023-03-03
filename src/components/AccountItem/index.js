e36d8174d944cdf8137ba355baee6c7ffa5348b7
lehung <lehung16082002@gmail.com>
2023-03-03 23:05:52 +0700

    Implement search popper in header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img
+        className={cx("avatar")}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/39911deb09b62b80810dec42c0722bbd~c5_300x300.webp?x-expires=1678028400&x-signature=ejrPfTHy28GS8eTfWrIJgfV41GU%3D"
+        alt=""
+      />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx("username")}>Nguyen Van A</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
